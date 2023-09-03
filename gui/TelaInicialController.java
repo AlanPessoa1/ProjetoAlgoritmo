803b800d1ad170a846dc05bcee1f4b736e72958d
Lucas-X-A <140546986+Lucas-X-A@users.noreply.github.com>
2023-09-03 17:37:36 -0300

    Add files via upload

---
+++ gui/TelaInicialController.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TelaInicialController {
+    @FXML
+    private Button btnJogar;
+
+    @FXML
+    private Button btnResolucao;
+
+    @FXML
+    void irTelaLabirinto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void itTelaResolucao(ActionEvent event) {
+
+    }
+
+}
---
+++ gui/TelaJogadorController.java
@@ -0,0 +1,40 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TelaJogadorController {
+
+    @FXML
+    private Button btnMoverBaixo;
+
+    @FXML
+    private Button btnMoverCima;
+
+    @FXML
+    private Button btnMoverDireita;
+
+    @FXML
+    private Button btnMoverEsquerda;
+
+    @FXML
+    void moverPraBaixo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moverPraCima(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moverPraDireita(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moverPraEsquerda(ActionEvent event) {
+
+    }
+}
