f2b9e6d9b97025481e4ca2c1c520902b04d74b83
Lucas-X-A <140546986+Lucas-X-A@users.noreply.github.com>
2023-09-04 22:00:17 -0300

    TelaInicialController.java

--- gui/TelaInicialController.java
+++ gui/TelaInicialController.java
@@ -2,9 +2,23 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class TelaInicialController {
+
+    Stage stage;
+    Scene scene;
+
     @FXML
     private Button btnJogar;
 
@@ -12,13 +26,20 @@ public class TelaInicialController {
     private Button btnResolucao;
 
     @FXML
-    void irTelaLabirinto(ActionEvent event) {
-
-    }
+    private AnchorPane painelLabirinto;
 
     @FXML
-    void itTelaResolucao(ActionEvent event) {
+    private TextArea txtLabirinto;
 
+
+    @FXML
+    void irTelaLabirinto(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("telaJogador.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("TELA JOGADOR");
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
