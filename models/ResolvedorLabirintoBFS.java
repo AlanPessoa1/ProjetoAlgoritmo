0e427b2ab63a26540b46e3cca652a5cf83d71036
Lucas-X-A <140546986+Lucas-X-A@users.noreply.github.com>
2023-09-08 14:35:45 -0300

    ResolvedorLabirintoBFS.java

--- models/ResolvedorLabirintoBFS.java
+++ models/ResolvedorLabirintoBFS.java
@@ -4,6 +4,9 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ResolvedorLabirintoBFS extends ResolvedorLabirinto {
+
+    private LinkedList<Integer> caminho = new LinkedList<>();
+
     public ResolvedorLabirintoBFS(Labirinto labirinto) {
         super(labirinto);
     }
@@ -11,6 +14,7 @@ public class ResolvedorLabirintoBFS extends ResolvedorLabirinto {
     // Algoritmo de busca em largura (BFS) para encontrar um caminho.
     public boolean BFS(int inicio, int fim) {
         boolean[] visitados = new boolean[labirinto.obterLargura() * labirinto.obterAltura()];
+        int[] pais = new int[labirinto.obterLargura() * labirinto.obterAltura()]; // Para rastrear os pais dos vértices
         Queue<Integer> fila = new LinkedList<>();
 
         visitados[inicio] = true;
@@ -18,11 +22,15 @@ public class ResolvedorLabirintoBFS extends ResolvedorLabirinto {
 
         while (!fila.isEmpty()) {
             int vertice = fila.poll();
-            if (vertice == fim) return true;
+            if (vertice == fim) {
+                reconstituirCaminho(pais, inicio, fim);
+                return true;
+            }
 
             for (int adj : grafo.getAdjVertices(vertice)) {
                 if (!visitados[adj]) {
                     visitados[adj] = true;
+                    pais[adj] = vertice; // O vértice atual é o pai do vértice adjacente
                     fila.add(adj);
                 }
             }
@@ -32,13 +40,26 @@ public class ResolvedorLabirintoBFS extends ResolvedorLabirinto {
 
     // Encontra a saída do labirinto usando BFS.
     public boolean temCaminhoParaSaida() {
-        int inicio = 2;
-        int fim = (labirinto.obterAltura() - 1) * labirinto.obterLargura() + labirinto.obterLargura() - 1;
+        int inicio = labirinto.obterEntrada();
+        int fim = labirinto.obterSaida();
         return BFS(inicio, fim);
     }
 
-    // Para uso em uma apresentação de console
+    // Reconstitui o caminho a partir dos pais e o armazena em 'caminho'
+    private void reconstituirCaminho(int[] pais, int inicio, int fim) {
+        int atual = fim;
+        while (atual != inicio) {
+            caminho.addFirst(atual);
+            atual = pais[atual];
+        }
+        caminho.addFirst(inicio);
+    }
+
     public String apresentarResultado() {
         return temCaminhoParaSaida() ? "Caminho encontrado!" : "Não há saída!";
+    }
+
+    public LinkedList<Integer> obterCaminho() {
+        return caminho;
     }
 }
